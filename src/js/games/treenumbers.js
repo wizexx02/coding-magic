01fe63bd841d325a3270b96328596df11f49a604
wizexx02 <dvladvlad11@gmail.com>
2025-04-18 22:01:08 +0300

    made final checks and done this task

--- src/js/games/treenumbers.js
+++ src/js/games/treenumbers.js
@@ -1,5 +1,5 @@
 export const maxNumberGameInit = () => {
-  const gameContainer = document.querySelector('.max-number-game');
+  const gameContainer = document.querySelector('.biggest-number');
 
   const gameHtml = `
       <div class="max-number-game__container">
@@ -9,24 +9,20 @@ export const maxNumberGameInit = () => {
             <input class="max-number-game__input" type="number" required name="number1" placeholder="Введіть число"/>
             <input class="max-number-game__input" type="number" required name="number2" placeholder="Введіть число"/>
             <input class="max-number-game__input" type="number" required name="number3" placeholder="Введіть число"/>
-            <button class="max-number-game__button" name="maxNumberButton">OK</button>
+          
           </form>
           <div class="max-number-game__result">
-            <p data-max_number_output>Найбільше число, яке ви ввели - (?)</p>
+            <p data-max_number_output>Найбільше число, яке ви ввели - (число)</p>
           </div>
         </div>
       </div>
     `;
 
   gameContainer.innerHTML = gameHtml;
-
-  const maxNumberForm = document.querySelector('[data-max_number_form]');
+  const form = document.querySelector('[data-max_number_form]');
   const outputElement = document.querySelector('[data-max_number_output]');
 
-  const handleSubmit = event => {
-    event.preventDefault();
-    const form = event.target;
-
+  const handleInput = () => {
     const num1 = parseFloat(form.number1.value);
     const num2 = parseFloat(form.number2.value);
     const num3 = parseFloat(form.number3.value);
@@ -38,8 +34,7 @@ export const maxNumberGameInit = () => {
 
     const max = Math.max(num1, num2, num3);
     outputElement.textContent = `Найбільше число, яке ви ввели - ${max}`;
-    form.reset();
   };
 
-  maxNumberForm.addEventListener('submit', handleSubmit);
+  form.addEventListener('input', handleInput);
 };
