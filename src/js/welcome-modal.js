130666e1418c8d2d620b36199b14ae83de54e6c9
wizexx02 <dvladvlad11@gmail.com>
2025-05-25 09:46:19 +0300

    add code

--- src/js/welcome-modal.js
+++ src/js/welcome-modal.js
@@ -1,6 +1,8 @@
 const welcomeForm = document.querySelector('[data-welcome-form]');
 const backdrop = document.querySelector('[data-welcome-backdrop]');
 const closer = document.querySelector('[data-welcome-close]');
+const userNameSpan = document.querySelector('.header__text-user');
+const nameInput = document.querySelector('#welcome-name');
 
 function modalCloser() {
   backdrop.classList.add('is-hidden');
@@ -14,11 +16,24 @@ function modalOpener() {
   document.body.classList.add('modal-open');
 }
 
-window.addEventListener('DOMContentLoaded', modalOpener);
+window.addEventListener('DOMContentLoaded', () => {
+  const savedName = localStorage.getItem('userName');
+  if (savedName) {
+    userNameSpan.textContent = savedName;
+  } else {
+    modalOpener();
+  }
+});
 
 closer.addEventListener('click', modalCloser);
 
 welcomeForm.addEventListener('submit', e => {
   e.preventDefault();
-  modalCloser();
+  const name = nameInput.value.trim();
+
+  if (name) {
+    localStorage.setItem('userName', name);
+    userNameSpan.textContent = name;
+    modalCloser();
+  }
 });
